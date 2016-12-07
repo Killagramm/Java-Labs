bdeee783c24ab759c335ab511e032fadd5037995
Killagramm <killagramm770412@gmail.com>
2016-12-07 17:57:13 -0500

    labs

---
+++ laba3/test.java
@@ -0,0 +1,32 @@
+
+
+import static javax.print.attribute.standard.MediaSizeName.A;
+
+
+public class test implements var03 {
+    public void logika(double d1, double d2, double d3) {
+
+        if (d1 > d3 && d2 > d3)
+            System.out.println("Сумма " + (d1 + d2));
+        else if (d1 > d2 && d3 > d2)
+            System.out.println("Сумма " + (d1 + d3));
+        else
+            System.out.println("Сумма " + (d2 + d3));
+    }
+    public void poka(double N) {
+        double k=1;
+        double summ=0;
+        while (summ<N){
+           summ+=1/k; k++;
+        }
+        System.out.println("Наибольшее целое " + k);
+        System.out.println("Сумма " + summ);
+    }
+    public void massivy(double[] A, int K, int L){
+    int summ=0;
+        for (int i=0;i<A.length;i++) {
+            if (i<K || i>L) summ+=A[i];
+        }
+        System.out.println("Сумма " + summ);
+    }
+}
---
+++ laba3/ura.java
@@ -0,0 +1,10 @@
+
+public class ura {
+    public static void main(String[] args) {
+        test t=new test();
+        t.logika(1,2,3);
+        t.poka(100);
+        double[] A ={2,4,6,8,10,12};
+        t.massivy(A ,2,3);
+    }
+}
